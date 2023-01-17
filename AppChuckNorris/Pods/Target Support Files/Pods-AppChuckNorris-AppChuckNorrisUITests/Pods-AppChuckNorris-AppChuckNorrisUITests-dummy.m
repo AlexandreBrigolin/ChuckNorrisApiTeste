c24f39aeef89a9b022f759683351f174cde2f267
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-01-17 15:52:35 -0300

    criando projete e adicionando alamofire

---
+++ AppChuckNorris/Pods/Target Support Files/Pods-AppChuckNorris-AppChuckNorrisUITests/Pods-AppChuckNorris-AppChuckNorrisUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppChuckNorris_AppChuckNorrisUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppChuckNorris_AppChuckNorrisUITests
+@end
