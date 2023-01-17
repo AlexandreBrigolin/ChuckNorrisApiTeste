bd79917b33c5a2ad6bf48149bdd21d9acdda7c7c
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-01-17 15:23:45 -0300

    criação do projeto

---
+++ AppChuckNorris/AppChuckNorris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppChuckNorris
+//
+//  Created by Alexandre Brigolin on 17/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
