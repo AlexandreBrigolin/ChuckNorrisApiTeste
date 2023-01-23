e1abd71169db6931ed6322eb637c749521ed15e2
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-01-23 11:02:20 -0300

    iniciando layout

---
+++ AppChuckNorris/AppChuckNorris/Feature/Home/Screen/HomeScreen.swift
@@ -0,0 +1,46 @@
+//
+//  HomeScreen.swift
+//  AppChuckNorris
+//
+//  Created by Alexandre Brigolin on 23/01/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubView()
+        self.configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubView(){
+        self.addSubview(tableView)
+    }
+    
+    public func congigTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource){
+        self.tableView.delegate = delegate
+        self.tableView.dataSource = dataSource
+    }
+    
+    private func configConstraints(){
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
