77889906ea4deff5d4f1a20964dba4039a02bf95
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-01-26 12:59:52 -0300

    criando telinha de piadas

---
+++ AppChuckNorris/AppChuckNorris/FeatureJoke/Joke/JokeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  JokeViewController.swift
+//  AppChuckNorris
+//
+//  Created by Alexandre Brigolin on 26/01/23.
+//
+
+import UIKit
+
+class JokeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ AppChuckNorris/AppChuckNorris/FeatureJoke/Joke/Screen/JokeViewControllerScreen.swift
@@ -0,0 +1,14 @@
+//
+//  JokeViewControllerScreen.swift
+//  AppChuckNorris
+//
+//  Created by Alexandre Brigolin on 26/01/23.
+//
+
+import UIKit
+
+class JokeViewControllerScreen: UIView {
+
+   
+
+}
